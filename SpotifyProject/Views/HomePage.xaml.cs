09067530be205c26f8b40fdf1fe0acd17e7fd0a7
20120473 <76527212+duongminhhieu@users.noreply.github.com>
2023-12-12 18:27:50 +0700

    done home page view

--- SpotifyProject/Views/HomePage.xaml.cs
+++ SpotifyProject/Views/HomePage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using SpotifyProject.Models;
+using SpotifyProject.Services;
+using SpotifyProject.ViewModels;
+using SpotifyProject.Views.Dialog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,14 +24,120 @@ namespace SpotifyProject.Views
     /// </summary>
     public partial class HomePage : Page
     {
+        private HomePageVM homePageVM;
         public HomePage()
         {
             InitializeComponent();
+            homePageVM = new HomePageVM();
         }
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
+            homePageVM.LoadPlaylists();
+            this.DataContext = homePageVM;
+            playlistSong.ItemsSource = homePageVM.PlaylistSong;
+            playlistVideo.ItemsSource = homePageVM.PlaylistVideo;
+            recentsList.ItemsSource = homePageVM.RecentViews;
+        }
 
+        private void panelChoosePlaylistSong_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Playlist? selectedPlaylist = ((FrameworkElement)sender).DataContext as Playlist;
+
+            if (selectedPlaylist != null)
+            {
+                NavigationService.Navigate(new PlaylistViewPage(selectedPlaylist));
+            }
+        }
+
+        private void panelChoosePlaylistVideo_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Playlist? selectedPlaylist = ((FrameworkElement)sender).DataContext as Playlist;
+
+            if (selectedPlaylist != null)
+            {
+                NavigationService.Navigate(new PlaylistViewPage(selectedPlaylist));
+            }
+        }
+
+        private void panelChooseRecent_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+            // select song and play
+            RecentView? selected = ((FrameworkElement)sender).DataContext as RecentView;
+
+            if (selected != null)
+            {
+                PlayerMedia.CurrentPlaylist = homePageVM.PlaylistService.GetPlaylist(selected.PlaylistId);
+                if (PlayerMedia.CurrentPlaylist.Type == PlaylistType.Song)
+                {
+                    var songIndex = 0;
+                    Song CurrentSong = null;
+                    foreach (var song in PlayerMedia.CurrentPlaylist.MediaItems)
+                    {
+                        if (song.Id == selected.MediaItemId)
+                        {
+                            CurrentSong = (Song?)song;
+                            break;
+                        }
+                        songIndex++;
+                    }
+
+                    if (CurrentSong != null)
+                    {
+                        PlayerMedia.CurrentSong = CurrentSong;
+                        PlayerMedia.CurrentSongIndex = songIndex;
+                        PlayerMedia.PlaySong(CurrentSong.Path);
+                    }
+                    else
+                    {
+                        // reload recents
+                        homePageVM.LoadRecents();
+                        recentsList.ItemsSource = homePageVM.RecentViews;
+                        MessageBox.Show("File not found!");
+
+                    }
+
+
+                }
+                else if (PlayerMedia.CurrentPlaylist.Type == PlaylistType.Video)
+                {
+                    var videoIndex = 0;
+                    Video CurrentVideo = null;
+                    foreach (var video in PlayerMedia.CurrentPlaylist.MediaItems)
+                    {
+                        if (video.Id == selected.MediaItemId)
+                        {
+                            CurrentVideo = (Video?)video;
+                            break;
+                        }
+                        videoIndex++;
+                    }
+
+                    if (CurrentVideo != null)
+                    {
+                        PlayerMedia.CurrentVideo = CurrentVideo;
+                        PlayerMedia.PauseSong();
+                        var dialog = new VideoViewDialog();
+                        bool? result = dialog.ShowDialog();
+                    }
+                    else
+                    {
+                        // reload recents
+                        homePageVM.LoadRecents();
+                        recentsList.ItemsSource = homePageVM.RecentViews;
+                        MessageBox.Show("File not found!");
+
+                    }
+                }
+
+            }
+
+        }
+
+        private void ListView_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            e.Handled = true;
         }
     }
 }
